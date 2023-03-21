82f6de79e4c164701a8d250ae0d85d49234e0e9d
Harshavardhan Nara <48521516+naraharsha224@users.noreply.github.com>
2023-03-21 08:05:44 +0530

    Create 3. Comments in Python.py

---
+++ 3. Comments in Python.py
@@ -0,0 +1,25 @@
+"""
+In Python, you can add comments to your code by using the "#" symbol. Comments are not executed by the interpreter, so they can be used to add explanations or reminders to your code without affecting its functionality.
+
+Here's an example of a Python code with comments:
+"""
+
+# This is a simple Python program
+# that prints "Hello, World!" to the console
+
+print("Hello, World!") # Print the greeting message
+
+"""
+In this example, the first two lines are comments that provide information about the purpose of the program. The third line is the actual code that prints the greeting message to the console. The comment at the end of the line explains what the code does.
+"""
+
+# You can also use multi-line comments in Python by enclosing your comments in triple quotes ("""..."""). Here's an example:
+
+"""
+This is a multi-line comment in Python.
+It can be used to add longer explanations to your code.
+"""
+
+print("Hello, World!") # Print the greeting message
+
+# In this example, the multi-line comment spans three lines and provides more detailed information about the program.

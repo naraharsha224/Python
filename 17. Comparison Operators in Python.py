6ef99b222224777347731f0e01b3ceeb589291c8
Harshavardhan Nara <48521516+naraharsha224@users.noreply.github.com>
2023-08-01 08:29:45 +0530

    Create 17. Comparison Operators in Python.py

---
+++ 17. Comparison Operators in Python.py
@@ -0,0 +1,35 @@
+'''
+In Python, comparison operators are used to compare two values and evaluate whether they are equal, not equal, greater than, less than, greater than or equal to, or less than or equal to. These operators return a Boolean value (True or False) depending on the comparison result. Here are the comparison operators in Python:
+
+Equal to (==): Checks if two values are equal.
+Not equal to (!=): Checks if two values are not equal.
+Greater than (>): Checks if the left operand is greater than the right operand.
+Less than (<): Checks if the left operand is less than the right operand.
+Greater than or equal to (>=): Checks if the left operand is greater than or equal to the right operand.
+Less than or equal to (<=): Checks if the left operand is less than or equal to the right operand.
+'''
+
+
+# Examples of comparison operators in Python
+a = 5
+b = 10
+
+# Equal to
+print(a == b)  # Output: False
+
+# Not equal to
+print(a != b)  # Output: True
+
+# Greater than
+print(a > b)   # Output: False
+
+# Less than
+print(a < b)   # Output: True
+
+# Greater than or equal to
+print(a >= b)  # Output: False
+
+# Less than or equal to
+print(a <= b)  # Output: True
+
+ 
